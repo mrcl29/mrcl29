d0b01b345b544650d02b070340335a65a086a671
mrcl29 <m.llobera@outlook.com>
2025-05-14 21:37:43 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: "/portfolio",
   server: {
     port: 3000 // Aquí cambias el puerto al que prefieras
   },
